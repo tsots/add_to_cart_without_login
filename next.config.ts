4136966a418d96e0102cb1052434345df4634878
tsots <mosalatsotetsi@gmail.com>
2024-11-12 17:51:43 +0200

    basePath init

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/add_to_cart_without_login",
   /* config options here */
   images: {
     remotePatterns: [
