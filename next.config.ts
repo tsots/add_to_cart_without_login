c6762605680fa904772766e77d2e0092d9f4f9b4
tsots <mosalatsotetsi@gmail.com>
2024-11-12 18:01:40 +0200

    workflow images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/add_to_cart_without_login",
+  output: "export",
+  reactStrictMode: true,
   /* config options here */
   images: {
     remotePatterns: [
